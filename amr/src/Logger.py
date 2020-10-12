e19593aba4476d0f2896cc7b2a9532976bd51a5d
Tanishq8597 <tjai8597@gmail.com>
2020-10-12 17:10:33 +1100

    Working logger and Arduino IMU

---
+++ amr/src/Logger.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+
+import rospy
+from amr.msg import grove_10dof
+from std_msgs.msg import Header
+
+rospy.init_node("IMU_Logger")
+log_time = Header()
+time_pub = rospy.Publisher("Propogation_Time",Header,queue_size=2)
+
+def IMU_CB(data):
+    current_time = rospy.get_rostime() 
+    log_time.stamp.secs = current_time.secs - data.header.stamp.secs
+    log_time.stamp.nsecs= current_time.nsecs - data.header.stamp.nsecs
+    # rospy.loginfo("%i sec %i nsec",log_time.stamp.secs-data.header.stamp.secs,log_time.stamp.nsecs-data.header.stamp.nsecs)
+    time_pub.publish(log_time)
+
+rospy.Subscriber("/IMU",grove_10dof,IMU_CB)
+rate=rospy.Rate(100)
+
+if __name__ == '__main__':
+    try:
+        while not rospy.is_shutdown():
+            rate.sleep()
+    except rospy.ROSInterruptException:
+        pass
