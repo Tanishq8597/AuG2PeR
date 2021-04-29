23b3a48443ff5f707af6ea8294c73de3a5826cb2
Tanishq8597 <tjai8597@gmail.com>
2021-04-29 11:22:23 +1000

    Moddfifed imu.ino

--- Arduino_Scripts/IMU/IMU.ino
+++ Arduino_Scripts/IMU/IMU.ino
@@ -3,6 +3,7 @@
 #include <sensor_msgs/Imu.h>
 
 #include "MPU9250.h"
+#include "I2Cdev.h"
 
 // SDA - D20 - White
 // SCL - D21 - Yellow
@@ -14,14 +15,15 @@ sensor_msgs::Imu imu_msg ;
 ros::Publisher IMU_AM_pub("/IMU", &imu_msg) ;
 
 // IMU
-MPU9250 IMU(Wire, 0x68) ;
-int IMU_status ;
-int16_t mx,my.mz
+MPU9250 IMU(0x68);
+int16_t ax, ay, az ;
+int16_t gx, gy, gz ;
+int16_t mx, my, mz ;
 
 
 void setup()
 {
-  IMU_node.initNode() ;
+  IMU_node.intialize();
   IMU_node.advertise(IMU_AM_pub);
   Serial.begin(500000) ;
   set_IMU();
@@ -29,17 +31,16 @@ void setup()
 
 void set_IMU()
 {
-  IMU_status = IMU.begin() ;
-  if (IMU_status > 0)
+  if (IMU.testConnection())
   {
-    // setting the accelerometer full scale range to +/-16G
-    IMU.setAccelRange(MPU9250::ACCEL_RANGE_16G);
-    // setting the gyroscope full scale range to +/-2000 deg/s
-    IMU.setGyroRange(MPU9250::GYRO_RANGE_2000DPS);
-    // setting DLPF bandwidth to 41 Hz
-    IMU.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_41HZ);
-    //  update rate = 1000/(1+SRD) => 250Hz
-    IMU.setSrd(3);
+    //    // setting the accelerometer full scale range to +/-16G
+    //    IMU.setAccelRange(MPU9250::ACCEL_RANGE_16G);
+    //    // setting the gyroscope full scale range to +/-2000 deg/s
+    //    IMU.setGyroRange(MPU9250::GYRO_RANGE_2000DPS);
+    //    // setting DLPF bandwidth to 41 Hz
+    //    IMU.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_41HZ);
+    //    //  update rate = 1000/(1+SRD) => 250Hz
+    //    IMU.setSrd(3);
 
     imu_msg.header.seq = 0 ;
   }
@@ -53,32 +54,42 @@ void set_IMU()
 
 void loop()
 {
-  if (IMU_status > 0)
+  if (IMU.testConnection())
   {
-//    IMU.readSensor() ;
+    //    IMU.readSensor() ;
     imu_msg.header.seq++ ;
 
     imu_msg.header.stamp = IMU_node.now() ;
     imu_msg.header.frame_id = "IMU" ;
 
-    IMU.getmotion9(
-      imu_msg.linear_acceleration.x,imu_msg.linear_acceleration.y,imu_msg.linear_acceleration.z,
-      imu_msg.angular_acceleration.x,imu_msg.angular_acceleration.y,imu_msg.angular_acceleration.z,
-      mx,my,mz)
+    IMU.getmotion9(ax, ay, az, gx, gy, gz, mx, my, mz) ;
 
-//    IMU_msg.accel_val[0] = IMU.getAccelerationX() ;
-//    IMU_msg.accel_val[1] = IMU.getAccelerationY() ;
-//    IMU_msg.accel_val[2] = IMU.getAccelerationZ() ;
-//
-//    IMU_msg.gyro_val[0] = IMU.getGyroX_rads() ;
-//    IMU_msg.gyro_val[1] = IMU.getGyroY_rads() ;
-//    IMU_msg.gyro_val[2] = IMU.getGyroZ_rads() ;
-//
-//    IMU_msg.mag_val[0] = IMU.getMagX_uT() ;
-//    IMU_msg.mag_val[1] = IMU.getMagY_uT() ;
-//    IMU_msg.mag_val[2] = IMU.getMagZ_uT() ;
 
-    IMU_msg.temp = IMU.getTemperature_C();
+    imu_msg.linear_acceleration.x = ax / 16384 ;
+    imu_msg.linear_acceleration.y = ay / 16384 ;
+    imu_msg.linear_acceleration.z = az / 16384 ;
+
+    imu_msg.angular_acceleration.x = gx * 250 / 32768;
+    imu_msg angular_acceleration.y = gy * 250 / 32768;
+    imu_msg.angular_acceleration.z = gz * 250 / 32768;
+    
+    mx = mx * 1200 / 4096;
+    my = my * 1200 / 4096;
+    mz = mz * 1200 / 4096;
+
+    //    IMU_msg.accel_val[0] = IMU.getAccelerationX() ;
+    //    IMU_msg.accel_val[1] = IMU.getAccelerationY() ;
+    //    IMU_msg.accel_val[2] = IMU.getAccelerationZ() ;
+    //
+    //    IMU_msg.gyro_val[0] = IMU.getGyroX_rads() ;
+    //    IMU_msg.gyro_val[1] = IMU.getGyroY_rads() ;
+    //    IMU_msg.gyro_val[2] = IMU.getGyroZ_rads() ;
+    //
+    //    IMU_msg.mag_val[0] = IMU.getMagX_uT() ;
+    //    IMU_msg.mag_val[1] = IMU.getMagY_uT() ;
+    //    IMU_msg.mag_val[2] = IMU.getMagZ_uT() ;
+
+    //    IMU_msg.temp = IMU.getTemperature_C();
 
     IMU_AM_pub.publish(&imu_msg) ;
     IMU_node.spinOnce();
